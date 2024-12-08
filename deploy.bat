90f3a0cda776d511ab0b53f2dfb5ad6f33888cf5
Muneeb Ur Rehman <137985020+Muneebsial0321@users.noreply.github.com>
2024-12-08 12:39:53 +0500

    deployed Γ£ö

---
+++ deploy.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "deployed ✔"
+git push origin main
--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { AdminModule } from './admin/admin.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
+import { UsersModule } from './users/users.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, ProductsModule, AdminModule, OrderModule, PaymentModule, UsersModule, MailModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
